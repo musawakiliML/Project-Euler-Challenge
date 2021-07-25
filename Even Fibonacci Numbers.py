c0eec04fdd1061e9a5d266df5ea53a71889d2172
Musa Adamu Wakili <musawakiliml@hotmail.com>
2021-07-26 00:02:04 +0100

    Update Even Fibonacci Numbers.py

--- Even Fibonacci Numbers.py
+++ Even Fibonacci Numbers.py
@@ -7,3 +7,12 @@ By starting with 1 and 2, the first 10 terms will be:
 By considering the terms in the Fibonacci sequence whose values do not exceed four million, 
 find the sum of the even-valued terms.
 '''
+a = 1
+b = 2
+counter = 0
+fib = 0
+
+while counter < 10:
+    fib = fib + a + b
+
+    counter += 1 
